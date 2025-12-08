1a422e219c6f665d597370e91f7c3b429dd9af54
Danilocpm <danilocarneirop@gmail.com>
2025-12-08 17:30:34 -0300

    refactor: streamline obfuscation logic in metro.config.js for improved readability

--- metro.config.js
+++ metro.config.js
@@ -6,34 +6,32 @@ const config = getDefaultConfig(__dirname);
 // Aplica obfuscação APENAS em produção
 const isProduction = process.env.NODE_ENV === 'production';
 
-if (isProduction) {
-  module.exports = withObfuscator(config, {
-    global: true,
-    filter: (filename) => {
-      return filename.includes("src/") || filename.includes("app/");
-    },
-    obfuscatorOptions: {
-      compact: true,
-      controlFlowFlattening: true,
-      controlFlowFlatteningThreshold: 0.75,
-      deadCodeInjection: true,
-      deadCodeInjectionThreshold: 0.4,
-      debugProtection: false,
-      disableConsoleOutput: true,
-      identifierNamesGenerator: 'hexadecimal',
-      log: false,
-      numbersToExpressions: true,
-      renameGlobals: false,
-      rotateStringArray: true,
-      selfDefending: true,
-      shuffleStringArray: true,
-      splitStrings: true,
-      stringArray: true,
-      stringArrayEncoding: ['base64', 'rc4'],
-      stringArrayThreshold: 0.75,
-      unicodeEscapeSequence: false
-    },
-  });
-} else {
-  module.exports = config;
-}
+module.exports = isProduction
+  ? withObfuscator(config, {
+      global: true,
+      filter: (filename) => {
+        return filename.includes("src/") || filename.includes("app/");
+      },
+      obfuscatorOptions: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: false,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: true,
+        renameGlobals: false,
+        rotateStringArray: true,
+        selfDefending: true,
+        shuffleStringArray: true,
+        splitStrings: true,
+        stringArray: true,
+        stringArrayEncoding: ['base64', 'rc4'],
+        stringArrayThreshold: 0.75,
+        unicodeEscapeSequence: false
+      },
+    })
+  : config;
